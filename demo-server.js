784028195187dae847ffeea455f820228abbeba2
Thongchai <67070233@kmitl.ac.th>
2026-07-09 13:36:02 +0700

    add about, contact, api:

--- demo-server.js
+++ demo-server.js
@@ -4,9 +4,26 @@ const PORT = 3000;
 const HOSTNAME = '127.0.0.1';
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify({ message: "Hello from the demo server!" }));
+  if (req.url === '/about'){
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('About Page\n');
+  } else if( req.url === '/contact'){
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('About Page\n');
+  } else if (req.url === '/api'){
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    const data = {
+        message: 'Hello from API',
+        timestamp: new Date().toISOString()
+    };
+    res.end(data.message + ' ' + data.timestamp);
+  } else{
+    res.statusCode = 200;
+    res.end('Hello World!\n');
+  }
 });
 
 server.listen(PORT, HOSTNAME, () => {
